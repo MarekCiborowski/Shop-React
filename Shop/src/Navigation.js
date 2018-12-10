12f17700fd416e01a302ef9110560033b656c21d
Marek Ciborowski <senthi236@gmail.com>
2018-12-10 16:35:28 +0100

    🔥🍺

--- Shop/src/Navigation.js
+++ Shop/src/Navigation.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom'
 
+
 class Navigation extends Component{
     render(){
-        const styles="btn btn-primary m-2"
-      return <nav className="navbar navbar-light bg-light">
         
-          <NavLink to='/' className={styles}>Home</NavLink>
-          <NavLink to='/cart' className={styles}>Cart</NavLink>
-          <NavLink to='/yourOrders' className={styles}>Your Orders</NavLink>
+      return <div class="container-box"><nav className="navbar-header">
+        
+          <NavLink to='/' className='btn-menu'>Home</NavLink>
+          <NavLink to='/cart' className='btn-menu'>Cart</NavLink>
+          <NavLink to='/yourOrders' className='btn-menu'>Your Orders</NavLink>
         
       </nav>
+      </div>
     }
   }
   export default Navigation
